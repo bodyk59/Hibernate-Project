6b231be893b433c730bbad97896165d1914806c9
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 08:10:47 +0300

    Updated MarathonService.java

--- src/main/java/com/softserve/academy/service/MarathonService.java
+++ src/main/java/com/softserve/academy/service/MarathonService.java
@@ -1,7 +1,15 @@
 package com.softserve.academy.service;
 
+import com.softserve.academy.entity.Marathon;
+
+import java.util.List;
+
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
 public interface MarathonService {
+    List<Marathon> getAll();
+    Marathon getMarathonById(Long id);
+    Marathon createOrUpdate(Marathon marathon);
+    void deleteMarathonById(Long id);
 }
