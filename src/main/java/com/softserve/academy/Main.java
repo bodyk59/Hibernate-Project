711e05da454c6fe32dfce6e88ee4ce60f7e7c658
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 09:01:48 +0300

    Added MarathonSessionFactory.java, Main.java

---
+++ src/main/java/com/softserve/academy/Main.java
@@ -0,0 +1,10 @@
+package com.softserve.academy;
+
+/**
+ * @author Bohdan Kurchak, Ruslan Pryimak
+ */
+public class Main {
+    public static void main(String[] args) {
+        //TODO: Add data to database
+    }
+}
---
+++ src/main/java/com/softserve/academy/utils/MarathonSessionFactory.java
@@ -0,0 +1,35 @@
+package com.softserve.academy.utils;
+
+import com.softserve.academy.entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * @author Bohdan Kurchak, Ruslan Pryimak
+ */
+public class MarathonSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    private MarathonSessionFactory() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure("src/main/resources/hibernate.cfg.xml");
+
+                configuration.addAnnotatedClass(Marathon.class);
+                configuration.addAnnotatedClass(Progress.class);
+                configuration.addAnnotatedClass(Sprint.class);
+                configuration.addAnnotatedClass(Task.class);
+                configuration.addAnnotatedClass(Users.class);
+
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                System.out.println(String.format("The exception was occur: %s", e));
+            }
+        }
+        return sessionFactory;
+    }
+}
