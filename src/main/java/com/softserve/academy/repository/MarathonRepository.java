81ba5ea5c344b565329af50063ae9aed35c672db
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 14:05:13 +0300

    Updated MarathonRepository.java

--- src/main/java/com/softserve/academy/repository/MarathonRepository.java
+++ src/main/java/com/softserve/academy/repository/MarathonRepository.java
@@ -1,40 +1,50 @@
 package com.softserve.academy.repository;
 
+import com.softserve.academy.entity.CustomEntity;
+import com.softserve.academy.utils.MarathonSessionFactory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
-public class MarathonRepository implements CrudRepository {
+@Repository
+public class MarathonRepository implements CrudRepository<CustomEntity, BigInteger> {
+
     @Override
-    public Object save(Object o) {
+    public <S extends CustomEntity> S save(S s) {
+        MarathonSessionFactory.getSessionFactory().openSession().saveOrUpdate(s);
+        return s;
+    }
+
+    @Override
+    public <S extends CustomEntity> Iterable<S> saveAll(Iterable<S> iterable) {
         return null;
     }
 
     @Override
-    public Iterable saveAll(Iterable iterable) {
-        return null;
+    public Optional<CustomEntity> findById(BigInteger bigInteger) {
+        return Optional.of(MarathonSessionFactory
+                .getSessionFactory()
+                .openSession()
+                .get(CustomEntity.class, bigInteger));
     }
 
     @Override
-    public Optional findById(Object o) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Object o) {
+    public boolean existsById(BigInteger bigInteger) {
         return false;
     }
 
     @Override
-    public Iterable findAll() {
+    public Iterable<CustomEntity> findAll() {
         return null;
     }
 
     @Override
-    public Iterable findAllById(Iterable iterable) {
+    public Iterable<CustomEntity> findAllById(Iterable<BigInteger> iterable) {
         return null;
     }
 
@@ -44,17 +54,19 @@ public class MarathonRepository implements CrudRepository {
     }
 
     @Override
-    public void deleteById(Object o) {
+    public void deleteById(BigInteger bigInteger) {
 
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(CustomEntity customEntity) {
+        MarathonSessionFactory.getSessionFactory()
+                .openSession()
+                .delete(customEntity);
     }
 
     @Override
-    public void deleteAll(Iterable iterable) {
+    public void deleteAll(Iterable<? extends CustomEntity> iterable) {
 
     }
 
