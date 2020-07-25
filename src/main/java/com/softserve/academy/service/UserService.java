18634d334336144333a61c678b53ab36ba8472f7
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 08:11:29 +0300

    Updated UserService.java

--- src/main/java/com/softserve/academy/service/UserService.java
+++ src/main/java/com/softserve/academy/service/UserService.java
@@ -1,7 +1,19 @@
 package com.softserve.academy.service;
 
+import com.softserve.academy.entity.Marathon;
+import com.softserve.academy.entity.Task;
+import com.softserve.academy.entity.Users;
+
+import java.util.List;
+
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
 public interface UserService {
+    List<Users> getAll();
+    Users getUserById(Long id);
+    Users createOrUpdateUser(Users user);
+    List<Users> getAllByRole(String role);
+    boolean addUserToMarathon(Users user, Marathon marathon);
+    boolean addUserToTask(Users user, Task task);
 }
