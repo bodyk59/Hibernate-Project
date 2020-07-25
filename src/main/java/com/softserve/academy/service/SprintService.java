3e2c87e8eceee353aa3230d8c21148c52f91ff46
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 08:11:11 +0300

    Updated SprintService.java

--- src/main/java/com/softserve/academy/service/SprintService.java
+++ src/main/java/com/softserve/academy/service/SprintService.java
@@ -1,7 +1,16 @@
 package com.softserve.academy.service;
 
+import com.softserve.academy.entity.Marathon;
+import com.softserve.academy.entity.Sprint;
+
+import java.util.List;
+
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
 public interface SprintService {
+    List<Sprint> getSprintsByMarathonId(Long marathonId);
+    boolean addSprintToMarathon(Sprint sprint, Marathon marathon);
+    boolean updateSprint(Sprint sprint);
+    Sprint getSprintById(Long id);
 }
