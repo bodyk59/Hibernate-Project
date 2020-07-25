a59815544cdb792889a6cc46c10c6c6901cb9b4b
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 08:11:20 +0300

    Updated TaskService.java

--- src/main/java/com/softserve/academy/service/TaskService.java
+++ src/main/java/com/softserve/academy/service/TaskService.java
@@ -1,7 +1,12 @@
 package com.softserve.academy.service;
 
+import com.softserve.academy.entity.Sprint;
+import com.softserve.academy.entity.Task;
+
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
 public interface TaskService {
+    Task addTaskToSprint(Task task, Sprint sprint);
+    Task getTaskById(Long id);
 }
