57dff042f81f7c443e873ede30ff8048adbf590b
Ruslan Pryimak <pryimak.rv@gmail.com>
2020-07-25 21:54:10 +0200

    Added repositories classes

---
+++ src/main/java/com/softserve/academy/repository/TaskRepository.java
@@ -0,0 +1,65 @@
+package com.softserve.academy.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Bohdan Kurchak, Ruslan Pryimak
+ */
+public class TaskRepository implements CrudRepository {
+    @Override
+    public Object save(Object o) {
+        return null;
+    }
+
+    @Override
+    public Iterable saveAll(Iterable iterable) {
+        return null;
+    }
+
+    @Override
+    public Optional findById(Object o) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterable findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable findAllById(Iterable iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
