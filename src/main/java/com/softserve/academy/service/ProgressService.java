93c7b71a2d9830bcc1e951dee419c4eb21e5c9b6
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-25 08:11:02 +0300

    Updated ProgressService.java

--- src/main/java/com/softserve/academy/service/ProgressService.java
+++ src/main/java/com/softserve/academy/service/ProgressService.java
@@ -1,7 +1,20 @@
 package com.softserve.academy.service;
 
+import com.softserve.academy.entity.Progress;
+import com.softserve.academy.entity.Task;
+import com.softserve.academy.entity.Users;
+
+import java.util.List;
+
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
 public interface ProgressService {
+    Progress getProgressById(Long id);
+    Progress addTaskForStudent(Task task, Users users);
+    Progress addOrUpdateProgress(Progress progress);
+    // TODO: in the specification the first parameter was TaskStatus -> why?
+    boolean setStatus(Task task);
+    List<Progress> allProgressByUserIdAndMarathonId(Long userId, Long marathonId);
+    List<Progress> allProgressByUserIdAndSprintId(Long userId, Long sprintId);
 }
