6dd9a7497ef790458d762cf2d4157734f8cd9f14
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-26 09:25:31 +0300

    Updated MarathonRepository.java

--- src/main/java/com/softserve/academy/repository/MarathonRepository.java
+++ src/main/java/com/softserve/academy/repository/MarathonRepository.java
@@ -1,68 +1,155 @@
 package com.softserve.academy.repository;
 
+import com.softserve.academy.entity.Marathon;
+import com.softserve.academy.utils.MarathonSessionFactory;
+import org.hibernate.Session;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.StreamSupport;
 
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
 @Repository
-public class MarathonRepository implements CrudRepository {
+public class MarathonRepository implements CrudRepository<Marathon, BigInteger> {
 
     @Override
-    public Object save(Object o) {
-        return null;
+    public <S extends Marathon> S save(S s) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(s);
+        session.getTransaction().commit();
+        session.close();
+        return s;
     }
 
     @Override
-    public Iterable saveAll(Iterable iterable) {
-        return null;
+    public <S extends Marathon> Iterable<S> saveAll(Iterable<S> iterable) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        List<S> marathonList = new ArrayList<>();
+        for (S s : iterable) {
+            session.save(s);
+            if (session.contains(s)) {
+                marathonList.add(s);
+            }
+        }
+        session.getTransaction().commit();
+        session.close();
+        return marathonList;
     }
 
     @Override
-    public Optional findById(Object o) {
+    public Optional<Marathon> findById(BigInteger bigInteger) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        Marathon marathon = null;
+        for (Marathon element : findAll()) {
+            if (Objects.equals(element.getId(), bigInteger)) {
+                marathon = element;
+                break;
+            }
+        }
+        session.getTransaction().commit();
+        session.close();
+        if (marathon != null) {
+            return Optional.of(marathon);
+        }
         return Optional.empty();
     }
 
     @Override
-    public boolean existsById(Object o) {
-        return false;
+    public boolean existsById(BigInteger bigInteger) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        boolean isExist = StreamSupport.stream(findAll().spliterator(), false)
+                .anyMatch(element -> Objects.equals(element.getId(), bigInteger));
+        session.getTransaction().commit();
+        session.close();
+        return isExist;
     }
 
     @Override
-    public Iterable findAll() {
-        return null;
+    public Iterable<Marathon> findAll() {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        List<Marathon> result = session.createQuery("from Marathon", Marathon.class).list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
     }
 
     @Override
-    public Iterable findAllById(Iterable iterable) {
-        return null;
+    public Iterable<Marathon> findAllById(Iterable<BigInteger> iterable) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        List<Marathon> result = new ArrayList<>();
+        for (BigInteger id : iterable) {
+            for (Marathon element : findAll()) {
+                if (Objects.equals(element.getId(), id)) {
+                    result.add(element);
+                }
+            }
+        }
+        session.getTransaction().commit();
+        session.close();
+        return result;
     }
 
     @Override
     public long count() {
-        return 0;
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        long number = ((Collection<Marathon>) findAll()).size();
+        session.getTransaction().commit();
+        session.close();
+        return number;
     }
 
     @Override
-    public void deleteById(Object o) {
-
+    public void deleteById(BigInteger bigInteger) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        for (Marathon element : findAll()) {
+            if (Objects.equals(element.getId(), bigInteger)) {
+                session.delete(element);
+            }
+        }
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Marathon marathon) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(marathon);
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
-    public void deleteAll(Iterable iterable) {
-
+    public void deleteAll(Iterable<? extends Marathon> iterable) {
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        for (Marathon element : iterable) {
+            session.delete(element);
+        }
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void deleteAll() {
-
+        Session session = MarathonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        for (Marathon element : findAll()) {
+            session.delete(element);
+        }
+        session.getTransaction().commit();
+        session.close();
     }
 }
