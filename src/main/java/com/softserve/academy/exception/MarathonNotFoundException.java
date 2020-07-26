d4f9adb462b27ae9e3f818718613da8c0ec80030
Ruslan Pryimak <pryimak.rv@gmail.com>
2020-07-26 15:11:01 +0200

    Updated Exception folder

--- src/main/java/com/softserve/academy/exception/MarathonNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.academy.exception;
-
-public class MarathonNotFoundException extends RuntimeException {
-    public MarathonNotFoundException(long id) {
-        super("Could not find marathon with id: " + id);
-    }
-}
