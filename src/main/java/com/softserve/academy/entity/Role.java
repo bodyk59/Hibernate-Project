492c7a1ead22f912dfadb46684d20efff7a24968
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-26 10:51:22 +0300

    Added realisation to MarathonServiceImplementation.java, deleted Roles

--- src/main/java/com/softserve/academy/entity/Role.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.academy.entity;
-
-/**
- * @author Bohdan Kurchak, Ruslan Pryimak
- */
-public enum Role {
-    MENTOR, STUDENT
-}
--- src/main/java/com/softserve/academy/service/implementation/MarathonServiceImplementation.java
+++ src/main/java/com/softserve/academy/service/implementation/MarathonServiceImplementation.java
@@ -4,39 +4,61 @@ import com.softserve.academy.entity.Marathon;
 import com.softserve.academy.repository.MarathonRepository;
 import com.softserve.academy.service.MarathonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * @author Bohdan Kurchak, Ruslan Pryimak
  */
-@Component
+@Service("marathonServiceImplementation")
 public class MarathonServiceImplementation implements MarathonService {
-    private MarathonRepository marathonRepository;
+    private final MarathonRepository marathonRepository;
 
     @Autowired
     public MarathonServiceImplementation(MarathonRepository marathonRepository) {
         this.marathonRepository = marathonRepository;
     }
 
+    @Transactional
     @Override
     public List<Marathon> getAll() {
-        return null;
+        return StreamSupport.stream(marathonRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
     }
 
+    @Transactional
     @Override
     public Marathon getMarathonById(Long id) {
-        return null;
+        return marathonRepository.findById(BigInteger.valueOf(id)).orElse(null);
     }
 
+    @Transactional
     @Override
     public Marathon createOrUpdate(Marathon marathon) {
-        return null;
+        if (Objects.nonNull(marathon.getId())) {
+            Marathon element = getMarathonById(marathon.getId().longValue());
+            if (Objects.nonNull(element)) {
+                element.setTitle(marathon.getTitle());
+                return marathonRepository.save(element);
+            }
+            //TODO: Throw exception, because the id already exist, but the marathon doesn`t
+        }
+       return marathonRepository.save(marathon);
     }
 
+    @Transactional
     @Override
     public void deleteMarathonById(Long id) {
-
+        if (Objects.nonNull(marathonRepository.findById(BigInteger.valueOf(id)))) {
+            marathonRepository.deleteById(BigInteger.valueOf(id));
+            return;
+        }
+        //TODO: Throw exception, because there isn`t needed marathon
     }
 }
